0a47926ba84a5cf6e52eac2673c9265e32f7ac1c
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-27 09:48:50 +0300

    added refresh token

--- server/app.js
+++ server/app.js
@@ -20,6 +20,26 @@ const users = [
 	}
 ];
 
+let refreshTokens = [];
+
+app.post('/api/refresh', (req, res) => {
+	// take the refresh token from the user
+	const refreshToken = req.body.token;
+
+	//send error if there is no token or it's invalid
+	if (!refreshToken) return res.status(401).json('You are not authenticated!');
+
+	// if everything is ok, create new access token, refresh token and send to user
+});
+
+const generateAccessToken = (user) => {
+	return jwt.sign({ id: user.id, isadmin: user.isAdmin }, process.env.SECRET_KEY, { expiresIn: '20s' });
+};
+
+const generateRefreshToken = (user) => {
+	return jwt.sign({ id: user.id, isadmin: user.isAdmin }, process.env.REFRESH_SECRET_KEY, { expiresIn: '20s' });
+};
+
 app.post('/api/login', (req, res) => {
 	const { username, password } = req.body;
 
@@ -28,8 +48,9 @@ app.post('/api/login', (req, res) => {
 	});
 
 	if (user) {
-		// Generate an access token
-		const accessToken = jwt.sign({ id: user.id, isAdmin: user.isAdmin }, 'mysecretkeyis1234', { expiresIn: '50s' });
+		generateAccessToken(user);
+		generateRefreshToken(user);
+
 		res.json({
 			id: user.id,
 			username: user.username,
